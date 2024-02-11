3385e62f5583c663096b22f721570c5c72fa488a
ShakedKelman <shaked.kelman@gmail.com>
2024-02-11 20:37:54 +0200

    added css changes and adjusted flex containers

--- codeFiles/tasks.js
+++ codeFiles/tasks.js
@@ -56,32 +56,35 @@ function resetForm() {
 // Function to render tasks on the page
 function renderTasks(tasks) {
     // Clear previous task output
-   let main_node = document.getElementById("mainTaskContainer")
-   main_node.innerHTML = "";
+   let mainNode = document.getElementById("mainTaskContainer")
+   mainNode.innerHTML = "";
 
     // Render each task
     tasks.forEach((task, index) => {
         // Create task div
         let taskDiv = document.createElement("div");
-        taskDiv.className = "task box";
+        taskDiv.className = "task box fw-light";
         let btn = document.createElement('button');
         btn.setAttribute('type', "button")
         btn.onclick = eraseTask; 
         btn.className="btn btn-default"
         btn.setAttribute('aria-label', "Left Align");
-        btn.innerHTML = `<span class=" glyphicon glyphicon-remove" aria-hidden="true"></span>`
+        btn.innerHTML = `<span class="glyphicon glyphicon-remove glyphicon-location" aria-hidden="true"></span>`
         taskDiv.appendChild(btn);
         taskInnerDiv = document.createElement('div');
         taskInnerDiv.className = 'box-inner-container';
         taskDiv.appendChild(taskInnerDiv);
         taskInnerDiv.innerHTML = `
-            <input type="checkbox" id="task${index}" ${task.checked ? 'checked' : ''} onchange="updateTask(${index}, this.checked)">
-            <label for="task${index}">${task.textOutput}</label><br>
+        <div class="checkbox-container">
+        <input type="checkbox" class="task-checkbox" id="task${index}" ${task.checked ? 'checked' : ''} onchange="updateTask(${index}, this.checked)">
+    </div>
+            <label for="task${index}" class="textOutput overflow-y-auto fw-medium">${task.textOutput}</label><br>
             <div class="TaskElseOutput">${task.elseOutput}</div>
         `;
-        main_node.appendChild(taskDiv);
+        mainNode.appendChild(taskDiv);
     });
 }
+
 
 // Function to update task status (checked/unchecked)
 function updateTask(index, checked) {
@@ -104,24 +107,24 @@ function deleteCheckedTasks() {
 }
 
 
-function eraseTask(evnt) {
-    let note = evnt.target.parentNode.parentNode
-    let chkbx = note.querySelector('input[type="checkbox"]');
-    if (chkbx !== null) {
-        chkbx.setAttribute('checked', true);
-        let taskId = chkbx.id;
+function eraseTask(event) {
+    let note = event.target.parentNode.parentNode
+    let checkBox = note.querySelector('input[type="checkbox"]');
+    if (checkBox !== null) {
+        checkBox.setAttribute('checked', true);
+        let taskId = checkBox.id;
         let taskIdNum = taskId.match(/[0-9]+/);
         updateTask(taskIdNum[0], true)
         deleteCheckedTasks();
-        init_tasks();
+        initTasks();
     }
 }
 
 
 // Function to initialize tasks on page load
- function init_tasks() {
+ function initTasks() {
     let tasks = getTasksFromLocalStorage();
     renderTasks(tasks);
 };
 
-window.onload = init_tasks;
+window.onload = initTasks;
